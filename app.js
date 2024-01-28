e0ce97f8f8875a05765117c6a699bbc64fbad6fd
vinaycvk <ch.vinay518@gmail.com>
2024-01-28 23:26:43 +0530

    First version of application moved into GitHub

---
+++ app.js
@@ -0,0 +1,89 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const wiki = require("./wiki.js");
+// …
+
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const catalogRouter = require("./routes/catalog");
+
+
+const compression = require("compression");
+const helmet = require("helmet");
+
+var app = express();
+
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  }),
+);
+
+
+app.use(compression()); 
+
+app.use("/wiki", wiki);
+
+// Set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+
+
+const dev_db_url =
+"mongodb+srv://cooluser:coolpassword@cluster0.o1dpxd4.mongodb.net/local_library?retryWrites=true&w=majority";
+
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use("/catalog", catalogRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ controllers/bookInstanceController.js
@@ -0,0 +1,172 @@
+const BookInstance = require("../models/bookinstance");
+const Book = require("../models/book");
+const asyncHandler = require("express-async-handler");
+const {body, validationResult} = require('express-validator');
+
+// Display list of all BookInstances.
+exports.bookinstance_list = asyncHandler(async (req, res, next) => {
+  
+  const allBookInstances = await BookInstance.find().populate("book").exec();
+  
+  res.render("bookinstance_list", {
+    title: "Book Instance List",
+    bookinstance_list: allBookInstances,
+  });
+});
+
+// Display detail page for a specific BookInstance.
+exports.bookinstance_detail = asyncHandler(async (req, res, next) => {
+   
+   const [bookInstances] = await Promise.all([
+        BookInstance.findById(req.params.id).populate("book").exec()
+   ]);
+
+   res.render("bookInstance_detail", {title: "Book Instance Detail", bookInstances: bookInstances});
+});
+
+// Display BookInstance create form on GET.
+exports.bookinstance_create_get = asyncHandler(async (req, res, next) => {
+  const allBooks = await Book.find({}, "title").sort({title: 1}).exec();
+
+  res.render("bookinstance_form",
+    {
+      title: "Create Instance",
+      books: allBooks,
+    });
+});
+
+// Handle BookInstance create on POST.
+exports.bookinstance_create_post = [
+
+  body("book", "Book name must be specified").trim().isLength({min: 1}).escape(),
+
+  body("imprint", "Imprint must be specified").trim().isLength({min: 1}).escape(),
+  body("status").escape(),
+
+  body("due_back", "Invalid Date").optional({values: "falsy"}).isISO8601().toDate(),
+
+
+
+  asyncHandler(async (req, res, next) => {
+
+    const errors = validationResult(req);
+
+    const bookInstance =  new BookInstance({
+      book: req.body.book,
+      imprint: req.body.imprint,
+      status: req.body.status,
+      due_back: req.body.due_back,
+    })
+
+    if (!errors.isEmpty()) {
+      // There are errors.
+      // Render form again with sanitized values and error messages.
+      const allBooks = await Book.find({}, "title").sort({ title: 1 }).exec();
+  
+      res.render("bookinstance_form", {
+        title: "Create BookInstance",
+        books: allBooks,
+        selected_book: bookInstance.book._id,
+        errors: errors.array(),
+        bookinstance: bookInstance,
+      });
+      return;
+    } else {
+      // Data from form is valid
+      await bookInstance.save();
+      res.redirect(bookInstance.url);
+    }
+  }),
+];
+// Display BookInstance delete form on GET.
+exports.bookinstance_delete_get = asyncHandler(async (req, res, next) => {
+     const bookInstance = await BookInstance.findById(req.params.id).populate('book').exec();
+
+     if (bookInstance === null){
+        res.redirect('/catalog/bookinstances');
+     };
+
+     res.render('bookinstance_delete', {
+      title: "Delete Instance",
+      bookinstance: bookInstance,
+     });
+});
+
+// Handle BookInstance delete on POST.
+exports.bookinstance_delete_post = asyncHandler(async (req, res, next) => {
+     await BookInstance.findByIdAndDelete(req.params.id);
+     res.redirect('/catalog/bookinstances');
+});
+
+// Display BookInstance update form on GET.
+exports.bookinstance_update_get = asyncHandler(async (req, res, next) => {
+  const [bookInstance, allBooks] = await Promise.all([
+    BookInstance.findById(req.params.id).populate("book").exec(),
+    Book.find(),
+  ]);
+
+  if (bookInstance === null) {
+    // No results.
+    const err = new Error("Book copy not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("bookinstance_form", {
+    title: "Update BookInstance",
+    books: allBooks,
+    selected_book: bookInstance.book._id,
+    bookinstance: bookInstance,
+  });
+});
+
+// Handle bookinstance update on POST.
+exports.bookinstance_update_post = [
+  // Validate and sanitize fields.
+  body("book", "Book must be specified").trim().isLength({ min: 1 }).escape(),
+  body("imprint", "Imprint must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("status").escape(),
+  body("due_back", "Invalid date")
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .toDate(),
+
+  // Process request after validation and sanitization.
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a BookInstance object with escaped/trimmed data and current id.
+    const bookInstance = new BookInstance({
+      book: req.body.book,
+      imprint: req.body.imprint,
+      status: req.body.status,
+      due_back: req.body.due_back,
+      _id: req.params.id,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors.
+      // Render the form again, passing sanitized values and errors.
+
+      const allBooks = await Book.find({}, "title").exec();
+
+      res.render("bookinstance_form", {
+        title: "Update BookInstance",
+        books: allBooks,
+        selected_book: bookInstance.book._id,
+        errors: errors.array(),
+        bookinstance: bookInstance,
+      });
+      return;
+    } else {
+      // Data from form is valid.
+      await BookInstance.findByIdAndUpdate(req.params.id, bookInstance, {});
+      // Redirect to detail page.
+      res.redirect(bookInstance.url);
+    }
+  }),
+];
